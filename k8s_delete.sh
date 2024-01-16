076d9316d7b8420a90e86dc33230ac5083ea6654
Endikson <81328446+Endikson@users.noreply.github.com>
2024-01-16 19:38:19 +0100

    Create k8s_delete.sh

---
+++ k8s_delete.sh
@@ -0,0 +1,6 @@
+kubectl delete deployments --all
+kubectl delete services --all
+kubectl delete pods --all
+#kubectl delete persistentvolumes --all
+
+#kubectl delete persistentvolumeclaim mysql-pvc
